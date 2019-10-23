60fc28bc8d0bd9b71d6767d39e180b63c930c93d
phueb <info@philhuebner.com>
2019-10-23 11:24:56 -0500

    fix typing

--- preppy/legacy.py
+++ preppy/legacy.py
@@ -5,7 +5,7 @@ multiple iterations were used in 2019 master's thesis of PH.
 
 """
 
-from typing import List
+from typing import List, Generator
 from cached_property import cached_property
 import numpy as np
 from functools import reduce
@@ -29,13 +29,14 @@ class Prep:
                  num_iterations: List[int],
                  batch_size: int,
                  context_size: int,
-                 num_evaluations: int
+                 num_evaluations: int,
+                 vocab: List[str] = None,
                  ):
 
-        self.num_parts = 2  # no need to manipulate this
+        self.num_parts = 2  # this project is for reference/demonstration only so no need to change
 
         tokens = reduce(iconcat, docs, [])  # flattens list of lists
-        self.store = TokenStore(tokens, self.num_parts, batch_size, context_size, num_types)
+        self.store = TokenStore(tokens, self.num_parts, batch_size, context_size, num_types, vocab)
         self.reverse = reverse
         self.num_types = num_types
         self.num_iterations = num_iterations
@@ -46,55 +47,53 @@ class Prep:
         print(f'Initialized Prep with {self.store.num_tokens} tokens')
 
     @cached_property
-    def num_mbs_in_part(self):
+    def num_mbs_in_part(self) -> int:
         result = self.num_windows_in_part / self.batch_size
         assert result.is_integer()
         return int(result)
 
     @cached_property
-    def num_iterations_list(self):
-        result = np.linspace(self.num_iterations[0], self.num_iterations[1],
-                             num=self.num_parts, dtype=np.int)
+    def num_iterations_list(self) -> List[int]:
+        result = list(np.linspace(self.num_iterations[0], self.num_iterations[1],
+                                  num=self.num_parts, dtype=np.int))
         return result
 
     @cached_property
-    def mean_num_iterations(self):
+    def mean_num_iterations(self) -> int:
         result = np.mean(self.num_iterations_list)
-        return result
-
-    @cached_property
-    def num_mbs_in_block(self):
-        result = self.num_mbs_in_part * self.mean_num_iterations
-        return result
-
-    @cached_property
-    def num_mbs_in_token_ids(self):
-        result = self.num_mbs_in_part * self.num_parts
+        assert float(result).is_integer()
         return int(result)
 
     @cached_property
-    def num_tokens_in_window(self):
+    def num_mbs_in_block(self) -> int:
+        return self.num_mbs_in_part * self.mean_num_iterations
+
+    @cached_property
+    def num_mbs_in_token_ids(self) -> int:
+        return self.num_mbs_in_part * self.num_parts
+
+    @cached_property
+    def num_tokens_in_window(self) -> int:
         return self.context_size + 1
 
     @cached_property
-    def num_windows_in_part(self):
-        result = self.num_tokens_in_part - self.num_tokens_in_window
-        return result
+    def num_windows_in_part(self) -> int:
+        return self.num_tokens_in_part - self.num_tokens_in_window
 
     @cached_property
-    def num_tokens_in_part(self):
+    def num_tokens_in_part(self) -> int:
         result = self.store.num_tokens / self.num_parts
         assert float(result).is_integer()
         return int(result)
 
     @cached_property
-    def stop_mb(self):
-        stop_mb = self.num_parts * self.num_mbs_in_block
-        return stop_mb
+    def stop_mb(self) -> int:
+        return self.num_parts * self.num_mbs_in_block
 
     @cached_property
-    def eval_mbs(self):
-        mbs_in_timepoint = int(self.stop_mb / self.num_evaluations)
+    def eval_mbs(self) -> List[int]:
+        assert self.stop_mb % self.num_evaluations == 0
+        mbs_in_timepoint = self.stop_mb // self.num_evaluations
         end = mbs_in_timepoint * self.num_evaluations + mbs_in_timepoint
         eval_mbs = list(range(0, end, mbs_in_timepoint))
         return eval_mbs
@@ -102,32 +101,37 @@ class Prep:
     # /////////////////////////////////////////////////////////////////// parts & windows
 
     @cached_property
-    def midpoint(self):
-        return len(self.store.num_tokens)
+    def midpoint(self) -> int:
+        res = self.store.num_tokens // 2
+        assert res * 2 == self.store.num_tokens
+        return res
 
     @cached_property
-    def part1(self):
+    def part1(self) -> List[int]:
         return self.store.token_ids[:self.midpoint]
 
     @cached_property
-    def part2(self):
+    def part2(self) -> List[int]:
         return self.store.token_ids[self.midpoint:]
 
     @cached_property
-    def reordered_parts(self):
+    def reordered_parts(self) -> List[List[int]]:
         if self.reverse:
             return [self.part2, self.part1]
         else:
             return [self.part1, self.part2]
 
-    def make_windows_mat(self, part, num_windows):
+    def make_windows_mat(self,
+                         part: List[int],
+                         num_windows: int
+                         ) -> np.ndarray:
         result = np.zeros((num_windows, self.num_tokens_in_window), dtype=np.int)
         for window_id in range(num_windows):
             window = part[window_id:window_id + self.num_tokens_in_window]
             result[window_id, :] = window
         return result
 
-    def gen_windows(self, iterate_once=False):
+    def gen_windows(self, iterate_once=False) -> Generator[np.ndarray, None, None]:
         """
         this was previously called "gen_ids", and it generated x, y rather than windows
         """
--- preppy/tokenstore.py
+++ preppy/tokenstore.py
@@ -2,7 +2,7 @@ from collections import Counter, OrderedDict
 from cached_property import cached_property
 from sortedcontainers import SortedSet
 from itertools import islice
-from typing import List
+from typing import List, Dict
 
 from preppy import config
 
@@ -19,22 +19,22 @@ class TokenStore(object):
                  batch_size: int,
                  context_size: int,
                  num_types: int,
-                 oov: str = config.Symbols.OOV,
                  _types: list = None,  # pass a vocabulary when tokens originate in test split
+                 oov: str = None,
                  ):
 
         self.num_parts = num_parts
         self.batch_size = batch_size
         self.context_size = context_size
         self.num_types = num_types
-        self.oov = oov
+        self.oov = oov or config.Symbols.OOV
 
         self._types = _types
 
         self.tokens_no_oov = self.prune(tokens)  # now tokens does not have to be stored in memory
         del tokens
 
-    def make_pruning_length(self, num_raw, max_num_docs=2048):
+    def make_pruning_length(self, num_raw, max_num_docs=2048) -> int:
         """
         Find length by which to prune corpora such that result is divisible by num_docs and
         such that the result of this division must be divisible by batch_size
@@ -53,7 +53,7 @@ class TokenStore(object):
                                          * num_words_in_window)
         return result
 
-    def prune(self, raw):
+    def prune(self, raw: List[str]) -> List[str]:
         num_raw = len(raw)
         pruning_length = self.make_pruning_length(num_raw)
         pruned = raw[:pruning_length]
@@ -63,14 +63,14 @@ class TokenStore(object):
     # /////////////////////////////////////////////////// properties
 
     @cached_property
-    def w2f_no_oov(self):
+    def w2f_no_oov(self) -> OrderedDict:
         c = Counter(self.tokens_no_oov)
         result = OrderedDict(
             sorted(c.items(), key=lambda item: (item[1], item[0]), reverse=True))  # order matters
         return result
 
     @cached_property
-    def types(self):
+    def types(self) -> List[str]:
         if self._types is None:
             most_freq_words = list(islice(self.w2f_no_oov.keys(), 0, self.num_types))
             sorted_words = sorted(most_freq_words[:-1] + [self.oov])
@@ -80,12 +80,12 @@ class TokenStore(object):
         return result
 
     @cached_property
-    def w2id(self):
+    def w2id(self) -> Dict[str, int]:
         result = {word: n for n, word in enumerate(self.types)}
         return result
 
     @cached_property
-    def tokens(self):
+    def tokens(self) -> List[str]:
         result = []
         for token in self.tokens_no_oov:
             if token in self.w2id:
@@ -95,21 +95,21 @@ class TokenStore(object):
         return result
 
     @cached_property
-    def token_ids(self):
+    def token_ids(self) -> List[int]:
         result = [self.w2id[token] for token in self.tokens]
         return result
 
     @cached_property
-    def oov_id(self):
+    def oov_id(self) -> int:
         result = self.w2id[self.oov]
         return result
 
     @cached_property
-    def num_tokens(self):
+    def num_tokens(self) -> int:
         result = len(self.tokens)
         return result
 
     @cached_property
-    def w2f(self):
+    def w2f(self) -> Dict[str, int]:
         result = Counter(self.tokens)
         return result
